4ed41be924340484d3c7a58f7c3ddcd12708d3b2
Xavier Flix <xavier@xfx.net>
2018-02-02 10:35:10 -0400

    Simplified evaluator

--- VisualFT/Evaluator.vb
+++ VisualFT/Evaluator.vb
@@ -2,6 +2,7 @@
 
 Public Class Evaluator
     Public Const Infinity As Double = 10 ^ 6
+    Public Const ToRad As Double = Math.PI / 180
 
     Public Delegate Function CustomFunctionDel(name As String, args As FunctionArgs)
 
@@ -38,30 +39,11 @@ Public Class Evaluator
                                                              args.Result = args.Parameters(2).Evaluate()
                                                          End If
                                                      Case "ToRad"
-                                                         args.Result = args.Parameters(0).Evaluate() * Math.PI / 180
+                                                         args.Result = args.Parameters(0).Evaluate() * ToRad
                                                      Case "Abs"
                                                          args.Result = Math.Abs(args.Parameters(0).Evaluate())
-                                                     Case "Osc"
-                                                         Dim f As Double = args.Parameters(0).Evaluate()
-                                                         Dim f2 As Double = f / 2
-                                                         Dim f4 As Double = f / 4
-                                                         Dim t As Double = Now.Ticks Mod f
-                                                         args.Result = If(t < f2, t - f4, f2 - t + f4) / f4
                                                      Case "Rnd"
                                                          args.Result = (New Random()).NextDouble()
-                                                     Case Else
-                                                         ' This is a cheap trick to handle arrays
-                                                         ' By default, this parser will interpret an array (such as A(3)) as a function call,
-                                                         ' so before giving up, we check if there's any parameter named A3.
-                                                         ' If such parameter exists, it means that the "user" has defined "A" as an array.
-                                                         Dim arrayName As String = $"{name}{args.Parameters(0).Evaluate()}"
-                                                         If mCustomParameters.ContainsKey(arrayName) Then
-                                                             args.Result = mCustomParameters(arrayName)
-                                                         Else
-                                                             If mCustomFunction IsNot Nothing Then
-                                                                 mCustomFunction(name, args)
-                                                             End If
-                                                         End If
                                                  End Select
                                              End Sub
 
@@ -71,9 +53,6 @@ Public Class Evaluator
                                                           args.Result = Math.PI
                                                       Case "e"
                                                           args.Result = Math.E
-                                                      Case "t"
-                                                          Dim t = Now.Ticks Mod 1000
-                                                          args.Result = If(t < 500, t - 250, 500 - t + 250)
                                                       Case Else
                                                           If mCustomParameters.ContainsKey(name) Then
                                                               args.Result = mCustomParameters(name)
